11bee71f35dc36ce4165c30c53396818da79e5a9
krampcg23 <krampcg23@gmail.com>
2019-03-20 16:42:18 -0600

    Naive

--- parallel_sparse_matvec.cu
+++ parallel_sparse_matvec.cu
@@ -1,4 +1,28 @@
+/* CSCI 563 Programming Assignment 3
+   Clayton Kramp
+*/
+
 #include <stdio.h>
+using namespace std;
+
+__global__ void naiveSpMV(float* t, float* b, float* ptr, float* data, float* ind) {
+
+    int tid = threadIdx.y;
+    int bid = blockIdx.y;
+    int myi = bid * 32 + tid;
+
+    if (myi < n) {
+        float temp = 0;
+        int lb = ptr[myi];
+        int ub = ptr[myi+1];
+        for (int j = lb; j < ub; j++) {
+            int index = ind[j];
+            temp += data[j] * b[index];
+        }
+        t[myi] = temp;
+    }
+}
+
 main (int argc, char **argv) {
   FILE *fp;
   char line[1024]; 
@@ -53,11 +77,49 @@ main (int argc, char **argv) {
   }
 
   // MAIN COMPUTATION, SEQUENTIAL VERSION
-  /*for (i=0; i<nr; i++) {                                                      
+  for (i=0; i<nr; i++) {                                                      
     for (j = ptr[i]; j<ptr[i+1]; j++) {
       t[i] = t[i] + data[j] * b[indices[j]];
     }
   }
-  */
+  
   // TODO: Compute result on GPU and compare output
+  float* deviceT;
+  cudaMalloc(&deviceT, nr * sizeof(float));
+  cudaMemcpy(deviceT, t, nr * sizeof(float), cudaMemcpyHostToDevice);
+
+  float* deviceB;
+  cudaMalloc(&deviceB, nc * sizeof(float));
+  cudaMemcpy(deviceB, b, nc * sizeof(float), cudaMemcpyHostToDevice);
+
+  float* devicePtr;
+  cudaMalloc(&devicePtr, (nr+1) * sizeof(float));
+  cudaMemcpy(devicePtr, ptr, (nr+1) * sizeof(float), cudaMemcpyHostToDevice);
+
+  float* deviceData;
+  cudaMalloc(&deviceData, n * sizeof(float));
+  cudaMemcpy(deviceData, data, n * sizeof(float), cudaMemcpyHostToDevice);
+
+  float* deviceIndices;
+  cudaMalloc(&deviceIndices, n * sizeof(float));
+  cudaMemcpy(deviceIndices, data, n * sizeof(float), cudaMemcpyHostToDevice);
+
+  dim3 threadsPerBlock(1,5,1);
+  dim3 numBlocks(1, 1, 1);
+  naiveSpMV<<<numBlocks, threadsPerBlock>>>(deviceT, deviceB, devicePtr, deviceData, deviceIndices);
+
+  newT = (float *) malloc(nr*sizeof(float));
+  cudaMemcpy(newT, deviceT, nr*sizeof(float));
+
+  for (int i = 0; i < nr; i++) {
+      cout << newT[i] - t[i] << endl;
+  }
+
+  cudaFree(deviceT);
+  cudaFree(deviceIndices);
+  cudaFree(devicePrt);
+  cudaFree(deviceData);
+  cudaFree(deviceB);
+
+
 }
